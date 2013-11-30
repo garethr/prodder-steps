e4706c256b27c1514ddd351f7d2465036d8c959a
Gareth Rushgrove <gareth@morethanseven.net>
2013-11-30 16:09:15 +0000

    allow headers to be optional when checking attributes

--- lib/prodder-steps/headers.rb
+++ lib/prodder-steps/headers.rb
@@ -4,6 +4,10 @@ def canonical_header(header)
   header.downcase.gsub('-', '_').to_sym
 end
 
+def header_includes_attribute?(header, attribute)
+  @response.headers[canonical_header(header)].to_s.should =~ /#{attribute}/
+end
+
 When(/^we request the site over "(.*?)"$/) do |protocol|
   @response = RestClient.get "#{protocol.downcase}://#{ENV['HOST']}"
 end
@@ -20,6 +24,10 @@ Then(/^the "(.*?)" header should be set$/) do |header|
   @response.headers.should have_key(canonical_header(header))
 end
 
-Then(/^the "(.*?)" header should have the "(.*?)" attribute$/) do |header, attribute|
-  @response.headers[canonical_header(header)].to_s.should =~ /#{attribute}/
+Then(/^(if present, )?the "(.*?)" header should have the "(.*?)" attribute$/) do |optional, header, attribute|
+  if optional
+    header_includes_attribute?(header, attribute) if @response.headers[canonical_header(header)]
+  else
+    header_includes_attribute?(header, attribute)
+  end
 end
--- lib/prodder-steps/version.rb
+++ lib/prodder-steps/version.rb
@@ -1,5 +1,5 @@
 module Prodder
   module Steps
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
